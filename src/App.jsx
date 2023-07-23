085b4b02f530898c8551be8999ff9cbc106cdf68
kolyas238 <kolyas238@gmail.com>
2023-07-23 16:25:07 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,174 +1,168 @@
+import {
+  useState,
+  useEffect
+} from "react";
 
-import { useState, useEffect } from "react";
-import './app.scss'
+import './app.scss';
 
 const Title = (props) => <h1>{props.text}</h1>;
 
-const CustomInput = ({ state, setInput }) => {
-    const inputHandler = (e) => {
-        setInput((prevInputValue) => ({
-            ...prevInputValue,
-            inputVal: Number(e.target.value)
-        }));
-    };
-
-    const radioHandler = (e) => {
-        setInput((prevInputValue) => ({
-            ...prevInputValue,
-            currency: e.target.id
-        }));
-    };
-
-    const curr = () => {
-        const selectedRadio = document.querySelector(
-            'input[name="currency"]:checked'
-        );
-        if (selectedRadio) {
-            return selectedRadio.id;
-        } else {
-            return null;
-        }
-    };
-
+const Buttons = ({ rates, selectFunc }) => {
+  const valutes = Object.values(rates);
+  const items = valutes.map((item, i) => {
     return (
-        <div className="wrapper">
-            <input
-                type="number"
-                className="input"
-                placeholder="Enter amount"
-                onChange={(e) => inputHandler(e)}
-            />
-            <input
-                className="inputRadio"
-                type="radio"
-                id="GBP"
-                name="currency"
-                onChange={(e) => radioHandler(e)}
-            />
-            <label tabIndex="1" htmlFor="GBP">GBP</label>
-            <input
-                className="inputRadio"
-                type="radio"
-                id="GEL"
-                name="currency"
-                onChange={(e) => radioHandler(e)}
-            />
-            <label tabIndex="1" htmlFor="GEL">GEL</label>
-            <input
-                className="inputRadio"
-                type="radio"
-                id="EUR"
-                name="currency"
-                onChange={(e) => radioHandler(e)}
-            />
-            <label tabIndex="1" htmlFor="EUR">EUR</label>
-            <input
-                className="inputRadio"
-                type="radio"
-                id="USD"
-                name="currency"
-                onChange={(e) => radioHandler(e)}
-            />
-            <label tabIndex="1" htmlFor="USD">USD</label>
-        </div>
+      <button
+        key={i}
+        className="btn"
+        id={item.CharCode}
+        onClick={(e) => selectFunc(e)}
+        title={item.Name}
+      >
+        {item.CharCode}
+      </button>
     );
+  });
+
+  return <>{items}</>;
+};
+
+const CustomInput = ({ rates, setInput }) => {
+  const inputHandler = (e) => {
+    setInput((prevInputValue) => ({
+      ...prevInputValue,
+      inputVal: Number(e.target.value)
+    }));
+  };
+
+  const radioHandler = (e) => {
+    setInput((prevInputValue) => ({
+      ...prevInputValue,
+      currency: e.target.id
+    }));
+  };
+
+  const labelsList = (arr) => {
+    const valutes = Object.values(arr);
+    const items = valutes.map((item, i) => {
+      return (
+        <>
+          <input
+            className="inputRadio"
+            type="radio"
+            id={item.CharCode}
+            name="currency"
+            onChange={(e) => radioHandler(e)}
+            title={item.Name}
+          />
+          <label title={item.Name} htmlFor={item.CharCode}>
+            {item.CharCode}
+          </label>
+        </>
+      );
+    });
+    return <>{items}</>;
+  };
+
+  return (
+    <div className="wrapper">
+      <input
+        type="number"
+        className="input"
+        placeholder="Enter amount"
+        onChange={(e) => inputHandler(e)}
+      />
+      {labelsList(rates)}
+    </div>
+  );
 };
 
 const App = ({ amount }) => {
-    const [rates, setRates] = useState("");
-    const [change, setChange] = useState("");
-    const [elem, setElem] = useState(false);
-    const [inputValue, setInputValue] = useState({
-        inputVal: 0,
-        currency: null
+  const [rates, setRates] = useState("");
+  const [change, setChange] = useState("");
+  const [elem, setElem] = useState(false);
+  const [inputValue, setInputValue] = useState({
+    inputVal: 0,
+    currency: null
+  });
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(
+          "https://www.cbr-xml-daily.ru/daily_json.js"
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data.Valute;
+      } catch (error) {
+        console.error(error);
+        return null;
+      }
+    };
+    getData().then((data) => {
+      setRates(data);
     });
+  }, []);
 
-    useEffect(() => {
-        const getData = async () => {
-            try {
-                const response = await fetch(
-                    "https://www.cbr-xml-daily.ru/daily_json.js"
-                );
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-                const data = await response.json();
-                return data;
-            } catch (error) {
-                console.error(error);
-                return null;
-            }
-        };
-        getData().then((data) => {
-            setRates(data);
-        });
-    }, []);
+  const getRate = (e, qty) => {
+    const val = e.target.id;
+    const curr = rates[val].Value;
 
-    const getRate = (e, qty) => {
-        const val = e.target.getAttribute("data-curr");
-        const curr = rates.Valute[val].Value;
-        setChange((qty / curr).toFixed(2) + " " + val);
-    };
+    setChange((qty / curr).toFixed(2) + " " + val);
+  };
 
-    const elemHandler = () => {
-        setElem(!elem);
-        setChange("");
-    };
+  const elemHandler = () => {
+    setElem(!elem);
+    setChange("");
+  };
 
-    const customValueHandler = (state, e) => {
-        const { inputVal, currency } = state;
+  const customValueHandler = (state, e) => {
+    const { inputVal, currency } = state;
 
-        if (!inputVal || !currency) {
-            alert("Not enough data!");
-            return null;
-        }
+    if (!inputVal || !currency) {
+      alert("Not enough data!");
+      return null;
+    }
 
-        const aimCurr = e.target.getAttribute("data-curr");
-        const fromVal = rates?.Valute?.[currency]?.Value;
-        const toVal = rates?.Valute?.[aimCurr]?.Value;
+    const aimCurr = e.target.id;
+    const fromVal = rates[currency]?.Value;
+    const toVal = rates[aimCurr]?.Value;
 
-        setChange(((inputVal * fromVal) / toVal).toFixed(2) + " " + aimCurr);
-    };
+    setChange(((inputVal * fromVal) / toVal).toFixed(2) + " " + aimCurr);
+  };
 
-    const selectFunc = (e) => {
-        if (elem) {
-            getRate(e, amount);
-        } else {
-            customValueHandler(inputValue, e);
-        }
-    };
+  const selectFunc = (e) => {
+    if (elem) {
+      getRate(e, amount);
+    } else {
+      customValueHandler(inputValue, e);
+    }
+  };
 
-    return (
-        <div className="box">
-            <Title text="Converter" />
-            {elem ? (
-                <span className="amount">{amount} RUB</span>
-            ) : (
-                <CustomInput state={inputValue} setInput={setInputValue} />
-            )}
-            <div className="frame">
-                <span className="typing-effect">
-                    You will get: <br />
-                    {change ? change : 0}
-                </span>
-            </div>
-            <button className="btn" data-curr="GBP" onClick={(e) => selectFunc(e)}>
-                1 GBP: {rates.Valute?.GBP?.Value} RUB
-            </button>
-            <button className="btn" data-curr="GEL" onClick={(e) => selectFunc(e)}>
-                1 GEL: {rates.Valute?.GEL?.Value} RUB
-            </button>
-            <button className="btn" data-curr="EUR" onClick={(e) => selectFunc(e)}>
-                1 EUR: {rates.Valute?.EUR?.Value} RUB
-            </button>
-            <button className="btn" data-curr="USD" onClick={(e) => selectFunc(e)}>
-                1 USD: {rates.Valute?.USD?.Value} RUB
-            </button>
-            <button className="btn" onClick={elemHandler}>
-                CUSTOM VALUE
-            </button>
-        </div>
-    );
+  return (
+    <div className="box">
+      <Title text="Converter" />
+      {elem ? (
+        <span className="amount">{amount} RUB</span>
+      ) : (
+        <CustomInput rates={rates} setInput={setInputValue} />
+      )}
+      <div className="frame">
+        <span className="typing-effect">
+          You will get: <br />
+          {change ? change : 0}
+        </span>
+      </div>
+      <div className="btnsWrap">
+        <Buttons rates={rates} selectFunc={selectFunc} />
+      </div>
+      <button className="btn btnLast" onClick={elemHandler}>
+        CUSTOM VALUE
+      </button>
+    </div>
+  );
 };
 
 export default App;
